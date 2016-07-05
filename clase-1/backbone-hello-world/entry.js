8fd0a41d4fe92a9469d1468b96b9100d075a6db3
Daniel Zavala <siedrix@gmail.com>
2016-07-05 18:55:24 -0500

    Demos

--- clase-1/backbone-hello-world/entry.js
+++ clase-1/backbone-hello-world/entry.js
@@ -1,1 +1,43 @@
-entry.js
+const $ = require('jquery')
+window.$ = $
+
+const _ = require('underscore')
+window._ = _
+
+const Backbone = require('Backbone')
+
+const ToDoListModel = Backbone.Model.extend({})
+
+const ToDoListCollection = Backbone.Collection.extend({
+	model: ToDoListModel
+})
+
+const toDoListCollection = new ToDoListCollection()
+window.toDoListCollection = toDoListCollection
+
+const ToDoListView = Backbone.View.extend({
+	collection: toDoListCollection,
+	initialize: function(){
+		this.listenTo(this.collection, 'add', this.render)
+		this.listenTo(this.collection, 'remove', this.render)
+		this.listenTo(this.collection, 'change', this.render)
+	},
+	render: function(){
+		var self = this
+
+		this.$el.html('')
+		this.collection.each(function(model){
+			self.$el.append('<div>'+model.get('title')+'</div>')
+		})
+	}
+})
+
+const toDoListView = new ToDoListView()
+
+$('body').append(toDoListView.$el)
+toDoListView.render()
+
+window.toDoListView = toDoListView
+
+toDoListCollection.add({title:'To Do 1'})
+toDoListCollection.add({title:'To Do 2'})
